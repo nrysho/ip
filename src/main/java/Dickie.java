eab0dbb7195233a03b0bc49d2d448483e0e2acd1
nrysho <neryssho@gmail.com>
2026-01-22 03:57:09 +0800

    Level 0

---
+++ src/main/java/Dickie.java
@@ -0,0 +1,15 @@
+public class Dickie {
+    public static void main(String[] args) {
+        greet();
+    }
+
+    public static void greet() {
+        String greeting = "____________________________________________________________\n" +
+                " Hey gorlll I'm Dickie\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Byee. See ya~\n" +
+                "____________________________________________________________";
+        System.out.println(greeting);
+    }
+}
