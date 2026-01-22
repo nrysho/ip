f95e00e3ba08eb61ca7d942c1382756a4d3c7f4c
nrysho <neryssho@gmail.com>
2026-01-23 00:51:16 +0800

    Level 2

--- src/main/java/Dickie.java
+++ src/main/java/Dickie.java
@@ -1,32 +1,46 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Dickie {
+    static ArrayList<String> TASKLIST = new ArrayList<>();
+
     public static void main(String[] args) {
         greet();
-        echo();;
+        Scanner scanner = new Scanner(System.in);
+        String message = scanner.nextLine();
+
+        while (!message.equals("bye")) {
+            if (message.equals("list")) {
+                list();
+                message = scanner.nextLine();
+            } else {
+                addTask(message);
+                message = scanner.nextLine();
+            }
+        }
+        bye();
     }
 
     public static void greet() {
-        String greeting = "____________________________________________________________\n" +
-                " Hey gorlll I'm Dickie\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________";
+        String greeting = "   Hey gorlll I'm Dickie\n" +
+                "   What can I do for you?";
         System.out.println(greeting);
     }
 
-    public static void echo() {
-        Scanner scanner = new Scanner(System.in);
-        String message = scanner.nextLine();
-        while (!message.equals("bye")) {
-            System.out.println("____________________________________________________________\n " +
-                    message + "\n" +
-                    "____________________________________________________________");
-            message = scanner.nextLine();
-        }
+    public static void bye () {
+        String greeting = "   Byee. See ya~\n";
+        System.out.println(greeting);
+    }
 
-        System.out.println("____________________________________________________________\n" +
-        " Byee. See ya~\n" +
-        "____________________________________________________________\n");
-        scanner.close();
+    public static void list() {
+        int number = 1;
+        for (String task: TASKLIST) {
+            System.out.println("   " + number + ". " + task);
+            number++;
+        }
+    }
+
+    public static void addTask(String task) {
+        TASKLIST.add(task);
+        System.out.println("   okie added: " + task);
     }
 }
